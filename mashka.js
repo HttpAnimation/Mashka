612ff71980e2f7d298c425ee3d8ad1cd4bdcc7a7
HttpAnimation <sahwndouglasyt@gmail.com>
2024-03-29 16:57:27 -0700

    localhost:port

--- mashka.js
+++ mashka.js
@@ -8,5 +8,5 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port localhost:${PORT}`);
 });
